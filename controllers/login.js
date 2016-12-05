d9487843d75ad94afa820afd2ae7d75c702d8781
nataliagarcia <nataliagarkcia@hotmail.com>
2016-12-04 22:40:48 -0500

    changes in home

--- controllers/login.js
+++ controllers/login.js
@@ -6,32 +6,37 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname + "../public/index.html"));
   });
 
+  console.log(__dirname + "../public/index.html");
+
   app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/home.html"));
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
 
     app.get("/aliento", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/aliento.html"));
+    res.sendFile(path.join(__dirname + "/../public/aliento.html"));
   });
 
 app.get("/armas", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/armas.html"));
+    res.sendFile(path.join(__dirname + "/../public/armas.html"));
   });
 
 app.get("/caidas", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/caidas.html"));
+    res.sendFile(path.join(__dirname + "/../public/caidas.html"));
   });
 
 app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/index.html"));
+    res.sendFile(path.join(__dirname + "/../public/index.html"));
   });
 
 app.get("/llaves", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/llaves.html"));
+    res.sendFile(path.join(__dirname + "/../public/llaves.html"));
   });
 
-app.get("/pumses", function(req, res) {
-    res.sendFile(path.join(__dirname + "../public/pumses.html"));
+app.get("/pumse", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/pumse.html"));
+  });
+app.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/signup.html"));
   });
 
 
